f7b44297045841cd4b6c822f66b6cd0f5504e413
CristianK25 <krahulikcristian@gmail.com>
2026-04-02 00:06:07 +0000

    boton eliminar y interface prop en tarjeta

--- src/components/TarjetaParticipante.tsx
+++ src/components/TarjetaParticipante.tsx
@@ -1,18 +1,21 @@
-// Definimos exactamente qué tipo de datos va a recibir nuestra tarjeta
 interface PropsTarjeta {
+  id: number;
   nombre: string;
   pais: string;
   modalidad: string;
   nivel: string;
-  tecnologias: string;
+  tecnologias: string[];
+  onEliminar: (id: number) => void;
 }
 
 export default function TarjetaParticipantes({
+  id,
   nombre,
   pais,
   modalidad,
   nivel,
   tecnologias,
+  onEliminar,
 }: PropsTarjeta) {
   return (
     <div className="border border-gray-200 shadow-sm rounded p-4 bg-white">
@@ -22,7 +25,15 @@ export default function TarjetaParticipantes({
       <p id="paisParticipante">{pais}</p>
       <p id="modalidadParticipante">Modalidad: {modalidad}</p>
       <p id="nivelParticipante">Nivel: {nivel}</p>
-      <p id="tecParticipante">{tecnologias}</p>
+      {/* SE JOINAN LOS ELEMENTOS DEL ARRAY CON UNA COMA Y ESPACIO PARA MOSTRARLOS LINDO */}
+      <p id="tecParticipante">{tecnologias.join(", ")}</p>
+
+      <button
+        onClick={() => onEliminar(id)}
+        className="mt-4 bg-red-600 text-white px-4 py-1 rounded hover:bg-red-700 transition"
+      >
+        Eliminar
+      </button>
     </div>
   );
 }
